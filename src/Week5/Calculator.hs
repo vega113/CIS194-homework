e31e10fb958168414c0a2d2ded3014e1a9756547
Yuri Zelikov <yurize@apache.org>
2018-10-05 23:36:58 +0300

    Week5 - Adds first calculator exercises.

--- src/Week5/Calculator.hs
+++ src/Week5/Calculator.hs
@@ -1,2 +1,55 @@
 module Week5.Calculator where
 
+import Week5.ExprT
+import Week5.Parser
+--import Week5.StackVM
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+instance Expr Integer where
+  lit i = i
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit i
+    | i <= 0 = False
+    | otherwise = True
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit i = MinMax i
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit i = Mod7 $ i `mod` 7
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
+
+reify :: ExprT -> ExprT
+reify = id
+
+
+eval :: ExprT -> Integer
+eval (Lit x) = x
+eval (Add expr1 expr2) = eval expr1 + eval expr2
+eval (Mul expr1 expr2) = eval expr1 * eval expr2
+
+evalStr :: String -> Maybe Integer
+evalStr str = fmap eval $ parseExp Lit Add Mul str
+
--- test/Week5/CalculatorSpec.hs
+++ test/Week5/CalculatorSpec.hs
@@ -6,6 +6,9 @@ module Week5.CalculatorSpec   ( main
 import Test.Hspec
 import Test.QuickCheck
 import Week5.Calculator
+import Week5.ExprT
+import Week5.Parser
+--import Week5.StackVM
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
 -- not needed for automatic spec discovery.
@@ -14,6 +17,20 @@ main = hspec spec
 
 spec :: Spec
 spec =
-  describe "balanced binary tree'" $ do
-    it "compares trees with Node and Node correctly left level == right level" $ do
-      True `shouldBe` True
+  describe "Calc" $ do
+    it "evaluates expression to 20" $
+      eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 20
+    it "evaluates string expression to 20" $
+     evalStr "10*2" `shouldBe` Just 20
+    it "converts Expr to ExprT" $
+     (mul (add (lit 2) (lit 3)) (lit 4) :: ExprT) `shouldBe` (Mul (Add (Lit 2) (Lit 3)) (Lit 4))
+    it "evaluates reified expression" $
+     (eval $ reify $ mul (add (lit 2) (lit 3)) (lit 4)) `shouldBe` 20
+    it "evaluates integer expression" $
+     ((mul (add (lit 2) (lit 3)) (lit 4))::Integer) `shouldBe` 20
+    it "evaluates boolean expression" $
+     ((mul (add (lit 2) (lit 3)) (lit 4))::Bool) `shouldBe` True
+    it "evaluates minmax expression" $
+     ((mul (add (lit 2) (lit 3)) (lit 4))::MinMax) `shouldBe` MinMax 3
+    it "evaluates mod7 expression" $
+     ((mul (add (lit 2) (lit 3)) (lit 4))::Mod7) `shouldBe` Mod7 6
